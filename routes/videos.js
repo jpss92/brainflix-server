58be1471b3a7e56f7c948ac7f3b5d69b5588ad18
Justin Swallow <132158985+jpss92@users.noreply.github.com>
2023-09-24 18:22:06 -0700

    motified axios, formatted code

--- routes/videos.js
+++ routes/videos.js
@@ -1,1 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const dataFilePath = "./data/video-details.json";
+let jsonData = [];
 
+try {
+    const data = fs.readFileSync(dataFilePath, "utf8");
+    jsonData = JSON.parse(data);
+} catch (error) {
+    console.error(error);
+}
+
+router.get('/', (req, res) => {
+    res.json(jsonData);
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const result = jsonData.find(item => item.id === id);
+
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(404).json({ error: 'Data not found' });
+    }
+});
+
+router.post('/', (req, res) => {
+    const newVideo = req.body;
+    jsonData.push(newVideo);
+
+    fs.writeFile(dataFilePath, JSON.stringify(jsonData, null, 2), (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: 'Error writing to file' });
+        }
+        res.status(201).json({ message: 'Video added successfully', data: newVideo });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,64 +1,14 @@
 const express = require("express");
-const fs = require("fs");
+const cors = require("cors");
 const app = express();
 const port = 8080;
-const cors = require("cors");
+const videosRoute = require("./routes/videos");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/videos', (req, res) => {
-    const filePath = './data/video-details.json';
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({error: 'Internal Server Error'})
-        }
-        try {
-            const jsonData =  JSON.parse(data);
-            res.json(jsonData)
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'JSON Parsing Error'})
-        }
-    });
-});
-
-const dataFilePath = "./data/video-details.json";
-let jsonData = [];
-try {
-    const data = fs.readFileSync(dataFilePath, 'utf8');
-    jsonData = JSON.parse(data);
-} catch (error) {
-    console.error(error);
-}
-
-app.get("/videos/:id", (req,res) => {
-    const id = req.params.id;
-    const result = jsonData.find(item => item.id === id);
-
-    if (result) {
-        res.json(result);
-    } else {
-        res.status(404).json({ error: 'Data not found'})
-    }
-});
-
-app.post("/videos", (req, res) => {
-
-    const newVideo = req.body;
-    jsonData.push(newVideo);
-
-    fs.writeFile(dataFilePath, JSON.stringify(jsonData, null, 2), (err) => {
-        if(err) {
-            console.log(err);
-            return res.status(500).json({error:'Error writing to file'});
-        }
-        res.status(201).json({message:'Video added successfully', data: newVideo});
-     });
-});
+app.use('/videos', videosRoute);
 
 app.listen(port, () => {
-    console.log("server is running on port 8080!");
+    console.log("Server is running on port 8080!");
 });
